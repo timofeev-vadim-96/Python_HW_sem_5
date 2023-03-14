7e276fe7ac7edc83f7b2b888cfe1d39557af8b94
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-03-14 19:56:18 +0300

    task 26 is ready

---
+++ task_26.py
@@ -0,0 +1,16 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень 
+# B с помощью рекурсии.
+
+# *Пример:*
+
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8 
+
+first_numb = 3
+second_numb = 5
+
+def degree(a,b):
+    if b == 1: return a
+    else:
+        return a*degree(a,b-1)
+print(degree(first_numb,second_numb))

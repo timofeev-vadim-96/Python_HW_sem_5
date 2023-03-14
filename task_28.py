55f7f5383d9b8374425dc27f3b40869ad72e7c02
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2023-03-14 20:12:19 +0300

    task 28

--- task_28.py
+++ task_28.py
@@ -5,3 +5,14 @@
 
 # 2 2
 # 4 
+
+first_numb = 15
+second_numb = 9
+
+def sum(a,b):
+    if a == 0: 
+        if b == 0:
+            return 0
+        else: return 1+sum(a,b-1)
+    else: return 1+sum(a-1,b)
+print(sum(first_numb,second_numb))
